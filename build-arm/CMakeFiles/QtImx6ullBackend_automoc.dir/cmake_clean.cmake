c8e751b28bb185ce6f45c1b59d6672b221905c3f
xiaohexin <alex.work.xian@gmail.com>
2025-10-12 19:08:14 -0700

    项目构建

--- build-arm/CMakeFiles/QtImx6ullBackend_automoc.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "QtImx6ullBackend_automoc.cpp"
-  "CMakeFiles/QtImx6ullBackend_automoc"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/QtImx6ullBackend_automoc.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
